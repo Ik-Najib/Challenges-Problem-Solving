dcd0949c793c0010617cf764cdb313c7bd2828d6
NAJIB <ikniounnajib@gmail.com>
2025-10-30 11:21:54 +0100

    final changes

--- ver de palindrom.js
+++ ver de palindrom.js
@@ -1,3 +1,13 @@
-const prompt = require("prompt-sync")()
-let mot = "level"
-let count = 0
+function estPalindrome(mot) {
+  mot = mot.toLowerCase();
+
+  for (let i = 0; i < mot.length / 2; i++) {
+    if (mot[i] !== mot[mot.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(estPalindrome("level"));
+console.log(estPalindrome("hello"));
